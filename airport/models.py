510b2b601a72119faee70cb22b03ca508e7b0b4f
Mykola <nickkozlov90@gmail.com>
2023-12-26 21:27:02 +0200

    Add image field to airplane model

--- airport/models.py
+++ airport/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.conf import settings
 from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
 from django.db import models
+from django.utils.text import slugify
 
 
 class Airport(models.Model):
@@ -19,11 +23,19 @@ class AirplaneType(models.Model):
         return f"{self.name}"
 
 
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.name)}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/airplanes/", filename)
+
+
 class Airplane(models.Model):
     name = models.CharField(max_length=255, unique=True)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
     airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
 
     @property
     def capacity(self) -> int:
